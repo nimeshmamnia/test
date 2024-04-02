36244dae310327e5e889ec1306cbc8b2a8f8253a
nimeshmamnia <Riaan123!@#>
2024-04-02 11:35:06 -0700

    echo "# test" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/nimeshmamnia/test.git git push -u origin main

---
+++ FindingFactors.py
@@ -0,0 +1,23 @@
+import math,sys
+while True:
+    res = input("Enter a a positive whole number to factor (or QUIT):  ")
+    if res.upper() == 'QUIT':
+        sys.exit()
+    if not res.isdecimal() or int(res) < 0:
+        continue
+    number = int(res)
+
+    factorial=[]
+    for i in range(1,int(math.sqrt(number))+1):
+        if number % i == 0:
+            factorial.append(i)
+            factorial.append(number // i)
+
+    factors = list(set(factorial))
+    factors.sort()
+    print(factors)
+    if len(factors) == 2:
+        print(number, 'is a prime number')
+    else:
+        print(number, 'is a composite number')
+
