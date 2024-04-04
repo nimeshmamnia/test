21c4898745444aca96f4bba1d800b5fc85a9bd86
nimeshmamnia <Riaan123!@#>
2024-04-04 10:18:30 -0700

    echo "# test" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/nimeshmamnia/test.git git push -u origin main

--- Rock-Paper-Scissor.py
+++ Rock-Paper-Scissor.py
@@ -1,20 +1,28 @@
-# Rock-Paper-Scissors Game:
-
-import random
-
-game = ['rock', 'paper', 'scissor']
-computer_choice = random.choice(game)
-
-user_input = input("Enter your choice (rock, paper, scissor): ")
-print("\nYour choice is: " + user_input + " and computers choice is: " + computer_choice)
-
-if user_input == computer_choice:
-    print("\nIt's a tie")
-elif user_input == 'rock' and computer_choice == 'scissor':
-    print('\nYou win')
-elif user_input == 'scissor' and computer_choice == 'paper':
-    print('\nYou win')
-elif user_input == 'paper' and computer_choice == 'rock':
-    print('\nYou win')
-else:
-    print("You loose, computer wins")
+import random, sys
+user_win = 0
+computer_win = 0
+tie = 0
+while True:
+    game = ['rock', 'paper', 'scissor']
+    computer_choice = random.choice(game)
+    user_choice = input("Enter your choice: (rock, paper, scissor) or QUIT- ")
+    print("\nYour choice is: " + user_choice + " and computers choice is: " + computer_choice)
+    if user_choice.upper() == 'QUIT':
+        print("Thanks for playing")
+        sys.exit()
+    elif user_choice == computer_choice:
+        print("It's a tie")
+        tie += 1
+    elif user_choice == 'rock' and computer_choice == 'scissor':
+        print("You win")
+        user_win += 1
+    elif user_choice == 'scissor' and computer_choice == 'paper':
+        print("You win")
+        user_win += 1
+    elif user_choice == 'paper' and computer_choice == 'rock':
+        print("You win")
+        user_win += 1
+    else:
+        print("You loose")
+        computer_win += 1
+    print('Score: You = ', user_win, "Computer = ",computer_win, "Tie = ", tie)
