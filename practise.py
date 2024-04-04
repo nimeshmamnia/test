6410c0275ba574176a0420ecad36f0e6dfde75ce
nimeshmamnia <Riaan123!@#>
2024-04-04 10:19:10 -0700

    echo "# test" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/nimeshmamnia/test.git git push -u origin main

--- practise.py
+++ practise.py
@@ -1,1 +1,20 @@
-print(100>>5)
+filename = input("Enter file Location: ")
+#filehandler = open(filename)
+names = {}
+with open(filename, 'r', encoding='utf-8', errors='ignore') as filehandler:
+    for lines in filehandler:
+        lines = lines.strip()
+        words = lines.split()
+        for word in words:
+            names[word] = names.get(word, 0) + 1
+
+big_count = None
+big_word = None
+for word, count in names.items():
+    if big_count is None or count > big_count:
+        big_word = word
+        big_count = count
+
+print(big_word, big_count)
+filehandler.close()
+
